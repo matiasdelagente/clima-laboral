b6f93d23ce4a76135f87f1f2040c1991b7dbd71c
pachirulo <pachirulo@gmail.com>
2015-11-06 20:25:44 -0300

    company send mail (WIP)

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -350,6 +350,8 @@ $scope.series1 = ['Serie 2015'];
   $scope.save = function(){
     $scope.processing = true;
     var url = $filter('encodeUri') ($scope.formCompany.name);
+    var psw = $scope.formUser.password;
+
     $scope.formCompany.url = 'http://system.fosteringtalent.com/' + url;
     //SET USER AS ADMIN
     $scope.formUser.admin = true;
@@ -362,7 +364,8 @@ $scope.series1 = ['Serie 2015'];
       
       companySrvc.save($scope.formCompany).success(function(companyData){
         $scope.formUser.company = companyData._id;
-
+        $scope.formUser.password = psw;
+        
         userSrvc.edit(userData._id, $scope.formUser).success(function(userResponse){
           $scope.processing = false;
           $scope.company = {};
@@ -481,94 +484,6 @@ $scope.series1 = ['Serie 2015'];
       $location.path('/users');
     });
   };
-
-  /*
-  $scope.sendMail = function(){
-
-    console.log($scope.formUser.email);
-    
-    var mailJSON ={
-      "key": "yXbOSbsEdGY5XDGY1G4JMw",
-      "template_name": "fts-invitacion",
-      "template_content": [
-      {
-        "name": "",
-        "content": ""
-      }
-      ],
-      "message": {
-        "html": "",
-        "text": "",
-        "subject": "",
-        "from_email": "no-responder@fosterintalent.com",
-        "from_name": "Soporte FTS",
-        "to": [
-        {
-          "email": $scope.formUser.email,
-          "name": $scope.formUser.user_name,
-          "type": "to"
-        }
-        ],
-        "important": false,
-        "track_opens": null,
-        "track_clicks": null,
-        "auto_text": null,
-        "auto_html": null,
-        "inline_css": null,
-        "url_strip_qs": null,
-        "preserve_recipients": null,
-        "view_content_link": null,
-        "tracking_domain": null,
-        "signing_domain": null,
-        "return_path_domain": null,
-        "global_merge_vars": [
-        {
-          "name": "user_name",
-          "content": $scope.formUser.user_name
-        },
-        {
-          "name": "username",
-          "content": $scope.formUser.username
-        },
-        {
-          "name": "password",
-          "content": $scope.formUser.password
-        },
-        {
-          "name": "company",
-          "content": $scope.formUser.company
-        },
-        {
-          "name": "URLEmpresa",
-          "content": $scope.formUser.URLEmpresa
-        }
-        ]
-      },
-      "async": false,
-      "ip_pool": "Main Pool"
-    };
-
-    var apiURL = "https://mandrillapp.com/api/1.0/messages/send-template.json";
-    $http.post(apiURL, mailJSON,{
-      headers: {
-
-      }}).
-    success(function(data, status, headers, config) {
-      alert('successful email send.');
-      $scope.form={};
-      console.log('successful email send.');
-      console.log('status: ' + status);
-      console.log('data: ' + data);
-      console.log('headers: ' + headers);
-      console.log('config: ' + config);
-    }).error(function(data, status, headers, config) {
-      console.log('error sending email.');
-      console.log('status: ' + status);
-    });
-
-
-  };
-  */
 
 })
 
--- routes/CompanyRoutes.js
+++ routes/CompanyRoutes.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Company = require('../models/CompanyModel.js');
 var User = require('../models/UserModel.js');
+var mandrill = require('mandrill-api/mandrill');
+var mandrill_client = new mandrill.Mandrill('yXbOSbsEdGY5XDGY1G4JMw');
 
 router.route('/companies')
   .get(function(req, res){
@@ -20,6 +22,77 @@ router.route('/companies')
       if (err) {
         res.send(err)
       } else {
+        console.log(req.body)
+        var mailJSON ={
+          "template_name": "fts-invitacion-empresa",
+          "template_content": [
+          {
+            "name": "",
+            "content": ""
+          }
+          ],
+          "message": {
+            "html": "",
+            "text": "",
+            "subject": "",
+            "from_email": "no-responder@fosterintalent.com",
+            "from_name": "Fostering Talent",
+            "to": [
+            {
+              "email": req.body.email,
+              "name": req.body.user_name,
+              "type": "to"
+            }
+            ],
+            "important": false,
+            "track_opens": null,
+            "track_clicks": null,
+            "auto_text": null,
+            "auto_html": null,
+            "inline_css": null,
+            "url_strip_qs": null,
+            "preserve_recipients": null,
+            "view_content_link": null,
+            "tracking_domain": null,
+            "signing_domain": null,
+            "return_path_domain": null,
+            "global_merge_vars": [
+            {
+              "name": "user_name",
+              "content": req.body.user_name
+            },
+            {
+              "name": "username",
+              "content": req.body.username
+            },
+            {
+              "name": "password",
+              "content": req.body.password
+            },
+            {
+              "name": "company",
+              "content": req.body.company
+            },
+            {
+              "name": "URLEmpresa",
+              "content": req.body.URLEmpresa
+            }
+            ]
+          },
+          "async": false,
+          "ip_pool": "Main Pool"
+        };
+
+        mandrill_client.messages.sendTemplate(mailJSON, 
+        function(result) {
+          console.log(result);
+        },function(e) {
+          // Mandrill returns the error as an object with name and message keys
+          console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+          // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+        });
+
+
         res.send(data)
       }
     })
