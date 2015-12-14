5f11d7e3a1be9246530482c2873a8f45c422afee
bueyfx <insua.francisco@buey-la.com.ar>
2015-12-14 19:51:49 -0300

    FIX graphs

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -730,7 +730,7 @@ $scope.series1 = ['Serie 2015'];
       ],
       {
         series: { pie: { show: true, innerRadius: 0.5, stroke: { width: 0 }, label: { show: true, threshold: 0.05 } } },
-        colors: ['#761c19','#f05050','#fad733','#27c24c','#4cae4c'],
+        colors: ['#4cae4c','#27c24c','#fad733','#f05050','#761c19'],
         grid: { hoverable: true, clickable: true, borderWidth: 0, color: '#ccc' },
         tooltip: false,
         tooltipOpts: { content: '%s: %p.0%' }
@@ -747,7 +747,7 @@ $scope.series1 = ['Serie 2015'];
       ],
       {
         series: { pie: { show: true, innerRadius: 0.5, stroke: { width: 0 }, label: { show: true, threshold: 0.05 } } },
-        colors: ['#761c19','#f05050','#fad733','#27c24c','#4cae4c'],
+        colors: ['#4cae4c','#27c24c','#fad733','#f05050','#761c19'],
         grid: { hoverable: true, clickable: true, borderWidth: 0, color: '#ccc' },
         tooltip: false,
         tooltipOpts: { content: '%s: %p.0%' }
@@ -757,7 +757,7 @@ $scope.series1 = ['Serie 2015'];
     var plot3 = $.plot("#graph3",
       [
         { data: [[0,7],[1,6.5],[2,12.5],[3,7],[4,9],[5,6],[6,11],[7,6.5],[8,8],[9,7]], label: 'Unique Visits', points: { show: true } },
-        { data: [["Recursos Humanos",4],[1,4.5],[2,7],[3,4.5],[4,3],[5,3.5],[6,6],[7,3],[8,4],[9,3]], label: 'Page Views', bars: { show: true, barWidth: 0.6, fillColor: { colors: [{ opacity: 0.2 }, { opacity: 0.4}] } } }
+        { data: [[0,4],[1,4.5],[2,7],[3,4.5],[4,3],[5,3.5],[6,6],[7,3],[8,4],[9,3]], label: 'Page Views', bars: { show: true, barWidth: 0.6, fillColor: { colors: [{ opacity: 0.2 }, { opacity: 0.4}] } } }
       ],
       {
         colors: [ '#23b7e5','#27c24c' ],
