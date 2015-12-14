0a2540ba962be6788cda64e51897cdeb6567f901
bueyfx <insua.francisco@buey-la.com.ar>
2015-12-14 19:47:33 -0300

    graphs

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -711,15 +711,15 @@ $scope.series1 = ['Serie 2015'];
 
     var plot = $.plot("#graph1",
       [
-        {label:'iPhone5S', data:40},
-        {label:'iPad Mini',data:10},
-        {label:'iPad Mini Retina',data:20},
-        {label:'iPhone4S',data:12},
-        {label:'iPad Air',data:18}
+        {label:'Excelente', data:10},
+        {label:'Muy Bueno',data:20},
+        {label:'Bueno',data:30},
+        {label:'Regular',data:30},
+        {label:'Deficiente',data:10}
       ],
       {
         series: { pie: { show: true, innerRadius: 0.5, stroke: { width: 0 }, label: { show: true, threshold: 0.05 } } },
-        colors: ['#7266ba','#23b7e5','#27c24c','#fad733','#f05050'],
+        colors: ['#761c19','#f05050','#fad733','#27c24c','#4cae4c'],
         grid: { hoverable: true, clickable: true, borderWidth: 0, color: '#ccc' },
         tooltip: false,
         tooltipOpts: { content: '%s: %p.0%' }
@@ -728,15 +728,15 @@ $scope.series1 = ['Serie 2015'];
 
     var plot2 = $.plot("#graph2",
       [
-        {label:'iPhone5S', data:40},
-        {label:'iPad Mini',data:10},
-        {label:'iPad Mini Retina',data:20},
-        {label:'iPhone4S',data:12},
-        {label:'iPad Air',data:18}
+        {label:'Excelente', data:20},
+        {label:'Muy Bueno',data:25},
+        {label:'Bueno',data:35},
+        {label:'Regular',data:15},
+        {label:'Deficiente',data:5}
       ],
       {
         series: { pie: { show: true, innerRadius: 0.5, stroke: { width: 0 }, label: { show: true, threshold: 0.05 } } },
-        colors: ['#7266ba','#23b7e5','#27c24c','#fad733','#f05050'],
+        colors: ['#761c19','#f05050','#fad733','#27c24c','#4cae4c'],
         grid: { hoverable: true, clickable: true, borderWidth: 0, color: '#ccc' },
         tooltip: false,
         tooltipOpts: { content: '%s: %p.0%' }
@@ -746,7 +746,7 @@ $scope.series1 = ['Serie 2015'];
     var plot3 = $.plot("#graph3",
       [
         { data: [[0,7],[1,6.5],[2,12.5],[3,7],[4,9],[5,6],[6,11],[7,6.5],[8,8],[9,7]], label: 'Unique Visits', points: { show: true } },
-        { data: [[0,4],[1,4.5],[2,7],[3,4.5],[4,3],[5,3.5],[6,6],[7,3],[8,4],[9,3]], label: 'Page Views', bars: { show: true, barWidth: 0.6, fillColor: { colors: [{ opacity: 0.2 }, { opacity: 0.4}] } } }
+        { data: [["Recursos Humanos",4],[1,4.5],[2,7],[3,4.5],[4,3],[5,3.5],[6,6],[7,3],[8,4],[9,3]], label: 'Page Views', bars: { show: true, barWidth: 0.6, fillColor: { colors: [{ opacity: 0.2 }, { opacity: 0.4}] } } }
       ],
       {
         colors: [ '#23b7e5','#27c24c' ],
